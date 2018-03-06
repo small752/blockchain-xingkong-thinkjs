17b2933eec421e11f4c7a4b64e055d2491f896d9
yujq <jqyu@ishanshan.com>
2018-03-06 16:21:29 +0800

    fixd: 修改线上配置

--- src/config/config.production.js
+++ src/config/config.production.js
@@ -1,6 +1,6 @@
 // production config, it will load in production enviroment
 module.exports = {
   workers: 0,
-  host: '118.89.237.160',
+  host: '172.21.0.10',
 	think_env: 'production',													//服务所处的环境
 };
