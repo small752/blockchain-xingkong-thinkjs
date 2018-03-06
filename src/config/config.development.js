b1a6f3bae7ca32114557b783d3eff56019511a7f
yujq <jqyu@ishanshan.com>
2018-03-06 15:32:03 +0800

    fixd: 修改配置

--- src/config/config.development.js
+++ src/config/config.development.js
@@ -1,5 +1,5 @@
 // production config, it will load in development enviroment
 module.exports = {
-	host: '10.0.0.198',
+	host: '118.89.237.160',
 	think_env: 'development',													//服务所处的环境
 };
