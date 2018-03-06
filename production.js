e777e65e0a6da391de7558f8fa22d02187b27095
yujq <jqyu@ishanshan.com>
2018-03-06 15:47:21 +0800

    fixd: 修改线上配置

--- production.js
+++ production.js
@@ -1,8 +1,12 @@
 const Application = require('thinkjs');
+const babel = require('think-babel');
 
 const instance = new Application({
   ROOT_PATH: __dirname,
   proxy: true, // use proxy
+  transpiler: [babel, {
+    presets: ['think-node']
+  }],
   env: 'production'
 });
 
